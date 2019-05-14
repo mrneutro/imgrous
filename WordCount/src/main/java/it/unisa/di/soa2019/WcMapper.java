4e3bd252e428839a8d985b4d5072cca7530bbd27
Sergio Shevchenko <shevchenkser@gmail.com>
2019-05-14 21:00:09 +0200

    CleanUP

---
+++ WordCount/src/main/java/it/unisa/di/soa2019/WcMapper.java
@@ -0,0 +1,21 @@
+package it.unisa.di.soa2019;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WcMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws
+            IOException, InterruptedException {
+        String line = value.toString();
+        String[] words = line.split(" ");
+        for (String word : words) {
+            context.write(new Text(word), new IntWritable(1));
+        }
+    }
+}
---
+++ WordCount/src/main/java/it/unisa/di/soa2019/WcReducer.java
@@ -0,0 +1,19 @@
+package it.unisa.di.soa2019;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class WcReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws
+            IOException, InterruptedException {
+        int sum = 0;
+        for (IntWritable value : values) {
+            sum += value.get();
+        }
+        context.write(key, new IntWritable(sum));
+    }
+}

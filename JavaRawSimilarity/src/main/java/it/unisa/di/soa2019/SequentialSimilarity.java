98c90e2077ff7e4b25d7c80da151e126ce9b7d32
Sergio Shevchenko <shevchenkser@gmail.com>
2019-06-08 18:07:37 +0200

    File main

--- JavaRawSimilarity/src/main/java/it/unisa/di/soa2019/SequentialSimilarity.java
+++ JavaRawSimilarity/src/main/java/it/unisa/di/soa2019/SequentialSimilarity.java
@@ -3,8 +3,8 @@ package it.unisa.di.soa2019;
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    private final int ITER_SIZE = 2;
+public class SequentialSimilarity {
+    private final int ITER_SIZE = Integer.MAX_VALUE;
     private Map<Long, Map<String, Long>> mapPartitioning;
     private Map<Long, Map<String, Double>> mapSimilarity;
     private FileInputStream inputStreamIn = null;
@@ -17,10 +17,10 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-        Main main = new Main();
-        main.setup(args);
-        main.partitioning();
-        main.similarity();
+        SequentialSimilarity sequentialSimilarity = new SequentialSimilarity();
+        sequentialSimilarity.setup(args);
+        sequentialSimilarity.partitioning();
+        sequentialSimilarity.similarity();
     }
 
     public void calcLine(Long groupID, String[] words) {
@@ -37,26 +37,26 @@ public class Main {
 
     public void partitioning() throws IOException {
         String[] values;
-        int iter=1;
+        int iter = 1;
         while (scIn.hasNextLine()) {
-            boolean validLine=true;
+            boolean validLine = true;
             String line = scIn.nextLine();
             values = line.split(",", 5);
             String[] words = values[3].replaceAll("[^\b a-zA-Z0-9]", "").toLowerCase().split("\\s+");
             String group_id = values[2];
-            if(words!=null && words.length>=1 && !words[0].isEmpty()){
+            if (words != null && words.length >= 1 && !words[0].isEmpty()) {
                 try {
                     Long.parseLong(group_id);
-                } catch (NumberFormatException e){
-                    validLine=false;
+                } catch (NumberFormatException e) {
+                    validLine = false;
                 }
 
-                if(validLine){
-                    if(!mapPartitioning.containsKey(Long.parseLong(group_id))){
+                if (validLine) {
+                    if (!mapPartitioning.containsKey(Long.parseLong(group_id))) {
                         mapPartitioning.put(Long.parseLong(group_id), new HashMap<String, Long>());
                     }
                     calcLine(Long.parseLong(group_id), words);
-                    if((iter%ITER_SIZE)==0){
+                    if ((iter % ITER_SIZE) == 0) {
                         updatePartFile();
                     }
                     iter++;
@@ -75,12 +75,12 @@ public class Main {
         filePart = new BufferedReader(new FileReader(partPath));
 
         while ((line = filePart.readLine()) != null) {
-            norma=new Double(0);
+            norma = new Double(0);
             values = line.split(",");
             Long groupID = Long.parseLong(values[0]);
             groupInFile.add(groupID);
 
-            if(mapPartitioning.containsKey(groupID)){
+            if (mapPartitioning.containsKey(groupID)) {
                 Map<String, Long> lineMap = new HashMap<>();
                 for (int i = 1; i < values.length - 1; i++) {
                     lineMap.put(values[i].split(":")[0], Long.parseLong(values[i].split(":")[1]));
@@ -89,7 +89,7 @@ public class Main {
                 Map<String, Long> mapWords = mapPartitioning.get(groupID);
                 Set<String> words = mapWords.keySet();
                 for (String word : words) {
-                    if(lineMap.containsKey(word)){
+                    if (lineMap.containsKey(word)) {
                         lineMap.put(word, mapWords.get(word) + lineMap.get(word));
                     } else {
                         lineMap.put(word, new Long(1));
@@ -108,14 +108,14 @@ public class Main {
             inputBuffer.append(line);
             inputBuffer.append('\n');
         }
-        for(Long groupID: mapPartitioning.keySet()){
-            norma= new Double(0);
-            if(!groupInFile.contains(groupID)){
+        for (Long groupID : mapPartitioning.keySet()) {
+            norma = new Double(0);
+            if (!groupInFile.contains(groupID)) {
                 StringBuilder lineBuilder = new StringBuilder();
                 lineBuilder.append(groupID);
                 Map<String, Long> mapGroup = mapPartitioning.get(groupID);
                 Set<String> words = mapGroup.keySet();
-                for(String word: words){
+                for (String word : words) {
                     lineBuilder.append(",").append(word).append(":").append(mapGroup.get(word));
                     norma += Math.pow(mapGroup.get(word), 2);
                 }
@@ -148,8 +148,8 @@ public class Main {
             Long groupID = Long.parseLong(values[0]);
             Map<String, Double> mapWords = new HashMap<>();
 
-            for (int i = 1; i < values.length-1; i++) {
-                mapWords.put(values[i].split(":")[0], Double.parseDouble(values[i].split(":")[1])/Double.parseDouble(values[values.length-1]));
+            for (int i = 1; i < values.length - 1; i++) {
+                mapWords.put(values[i].split(":")[0], Double.parseDouble(values[i].split(":")[1]) / Double.parseDouble(values[values.length - 1]));
             }
 
             mapSimilarity.put(groupID, mapWords);
@@ -164,9 +164,9 @@ public class Main {
             for (int j = 0; j <= i; j++) {
                 iteratorJ.next();
             }
-            for (int j = i+1; j < groupIDs.size(); j++) {
+            for (int j = i + 1; j < groupIDs.size(); j++) {
                 Long groupJ = iteratorJ.next();
-                Double similarity=new Double(0);
+                Double similarity = new Double(0);
                 Map<String, Double> mapI = mapSimilarity.get(groupI);
                 Map<String, Double> mapJ = mapSimilarity.get(groupJ);
                 Iterable<String> wordsI = mapI.keySet();
@@ -174,12 +174,12 @@ public class Main {
                 StringBuilder lineBuilder = new StringBuilder();
                 lineBuilder.append("(").append(groupI).append(",").append(groupJ).append(")").append("\t");
 
-                for(String word: wordsI){
-                    if(mapJ.containsKey(word)){
+                for (String word : wordsI) {
+                    if (mapJ.containsKey(word)) {
                         similarity += mapI.get(word) * mapJ.get(word);
                     }
                 }
-                if(similarity!=0){
+                if (similarity != 0) {
                     lineBuilder.append(similarity);
                     line = lineBuilder.toString();
                     inputBuffer.append(line);
@@ -203,6 +203,8 @@ public class Main {
         partPath = args[2];
         try {
             mapPartitioning = new HashMap<>();
+            File inPathFile = new File(inPath);
+            inPathFile.createNewFile();
             inputStreamIn = new FileInputStream(inPath);
             scIn = new Scanner(inputStreamIn, "UTF-8");
             inputStreamOut = new FileInputStream(outPath);

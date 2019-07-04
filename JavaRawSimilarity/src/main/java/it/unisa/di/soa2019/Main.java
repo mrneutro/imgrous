45770db242ecf258ee95887daf8959b0db78dc3b
linuxarto <lino.sarto@gmail.com>
2019-07-04 15:03:38 +0200

    rimosso inputSplit dal Main e inserita lettura inFile da Thread

--- JavaRawSimilarity/src/main/java/it/unisa/di/soa2019/Main.java
+++ JavaRawSimilarity/src/main/java/it/unisa/di/soa2019/Main.java
@@ -1,9 +1,4 @@
 package it.unisa.di.soa2019;
-import org.tartarus.snowball.SnowballStemmer;
-import org.tartarus.snowball.ext.englishStemmer;
-import org.tartarus.snowball.ext.italianStemmer;
-import org.tartarus.snowball.ext.russianStemmer;
-import org.tartarus.snowball.ext.spanishStemmer;
 
 import java.io.*;
 import java.util.*;
@@ -16,10 +11,7 @@ public class Main implements PartitioningInterface, SimilarityInterface {
     private AtomicInteger numThread;
     private PartitioningRunnable partitioningRunnable;
     private SimilarityRunnable similarityRunnable;
-//    public final int ITER_SIZE = 10000000;
-    public final int ITER_SIZE = 100000;
     private Map<Long, Map<String, Long>> mapPartitioning;
-//    private Map<Long, Map<String, Double>> mapSimilarity;
     private List<Map<Long, Map<String, Long>>> mapPartitioningList;
     private FileInputStream inputStreamIn = null;
     private Scanner scIn = null;
@@ -31,8 +23,9 @@ public class Main implements PartitioningInterface, SimilarityInterface {
     private Map<Integer, List<String>> mapSimilarityLines;
 
     private File filePart;
+    private File inFile;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Main main = new Main();
         System.out.println("Main starts....\n");
 
@@ -66,14 +59,10 @@ public class Main implements PartitioningInterface, SimilarityInterface {
 
     }
 
-    public void threadPartitioningStart(int numberOfProcessors) throws IOException {
-        Map<Integer, List<List<String>>> mapPartitioningLists = inputSplit(numberOfProcessors);
-
+    public void threadPartitioningStart(int numberOfProcessors) {
         numThread = new AtomicInteger(numberOfProcessors);
         for (int i = 0; i < numberOfProcessors; i++) {
-            List<List<String>> mapPartitioningListsClone = new ArrayList<>();
-            mapPartitioningListsClone.addAll(mapPartitioningLists.get(i));
-            partitioningRunnable = new PartitioningRunnable(this, mapPartitioningListsClone);
+            partitioningRunnable = new PartitioningRunnable(this, inFile);
             Thread thread = new Thread(partitioningRunnable);
             thread.start();
         }
@@ -87,88 +76,7 @@ public class Main implements PartitioningInterface, SimilarityInterface {
     }
 
 
-    public Map<Integer, List<List<String>>> inputSplit(int numSplits) throws IOException {
-        String[] values;
-
-        long numLine = 0;
-        Map<Integer, List<List<String>>> mapPartitioningLists = new HashMap<>();
-        for (int i = 0; i < numSplits; i++) {
-            mapPartitioningLists.put(i, (List) new ArrayList<>());
-        }
-        while (scIn.hasNextLine()) {
-            List<String> wordList = new ArrayList<>();
-            String line = scIn.nextLine();
-            boolean validLine = true;
-            values = line.split(",");
-            String lang = null;
-
-            if (values.length > 4) {
-                if (values.length > 5) {
-                    lang = values[values.length - 1];
-                    for (int i = 4; i < values.length - 1; i++) {
-                        values[3] = values[3] + values[i]; // TODO use string builder here
-                    }
-                } else {
-                    try {
-                        lang = values[4];
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        lang = "unknown";
-                    }
-                }
-
-                String[] words = values[3].replaceAll("\\n", " ").replaceAll("[^\b a-zA-Z0-9'а-яА-Я]", "").trim().toLowerCase().split(" ");
-                String group_id = values[2];
-                if (words != null && words.length >= 1) {
-                    try {
-                        Long.parseLong(group_id);
-                    } catch (NumberFormatException e) {
-                        validLine = false;
-//                        e.printStackTrace();
-                    }
-                    if (validLine) {
-                        wordList.add(group_id);
-                        wordList.add(lang);
-                        wordList.addAll(Arrays.asList(words));
-                        mapPartitioningLists.get(new Long(numLine % numSplits).intValue()).add(wordList);
-                    }
-                    if (numLine % ITER_SIZE == 0) {
-                        Map<Integer, List<List<String>>> mapPartitioningInit = new HashMap<>();
-                        for (Map.Entry<Integer, List<List<String>>> entryMapPartitioningList : mapPartitioningLists.entrySet()) {
-                            StringBuffer inputBuffer = new StringBuffer();
-                            Integer threadID = entryMapPartitioningList.getKey();
-
-                            File filePartThread = new File(partPath + threadID);
-                            if (!filePartThread.exists()) {
-                                filePartThread.createNewFile();
-                            } else if (numLine < ITER_SIZE) {
-                                filePartThread.delete();
-                                filePartThread.createNewFile();
-                            }
-
-                            for (List<String> wordGroup : entryMapPartitioningList.getValue()){
-                                StringBuilder lineBuilder = new StringBuilder();
-                                for (String word : wordGroup) {
-                                    lineBuilder.append(word).append(",");
-                                }
-                                inputBuffer.append(lineBuilder.toString());
-                                inputBuffer.append("\n");
-                            }
-                            FileOutputStream filePartOut = new FileOutputStream(filePartThread, true);
-                            filePartOut.write(inputBuffer.toString().getBytes());
-                            filePartOut.close();
-                            mapPartitioningInit.put(entryMapPartitioningList.getKey(), (List) new ArrayList<>());
-                        }
-                        mapPartitioningLists = new HashMap<>(mapPartitioningInit);
-                    }
-                }
-                numLine++;
-            }
-        }
-        return mapPartitioningLists;
-    }
-
-    public Map<Integer, List<String>> similaritySplit() throws IOException {
+    public Map<Integer, List<String>> similaritySplit() {
         mapSimilarityLines = new HashMap<>();
         String line;
         Integer numLine = 0;
@@ -198,10 +106,11 @@ public class Main implements PartitioningInterface, SimilarityInterface {
         outPath = args[1];
         partPath = args[2];
         try {
+            inFile = new File(inPath);
             callbacks = new AtomicInteger(0);
             mapPartitioningList = new ArrayList<>();
             mapPartitioning = new HashMap<>();
-            inputStreamIn = new FileInputStream(inPath);
+            inputStreamIn = new FileInputStream(inFile);
             scIn = new Scanner(inputStreamIn, "UTF-8");
             PrintWriter writer;
             filePart = new File(partPath);
--- JavaRawSimilarity/src/main/java/it/unisa/di/soa2019/PartitioningRunnable.java
+++ JavaRawSimilarity/src/main/java/it/unisa/di/soa2019/PartitioningRunnable.java
@@ -11,6 +11,7 @@ import java.util.*;
 
 public class PartitioningRunnable implements Runnable {
 
+    private File inputFile;
     private long threadID;
     private boolean calcMap = false;
     private boolean mapCmp = false;
@@ -20,11 +21,11 @@ public class PartitioningRunnable implements Runnable {
     private Map<Long, Map<String, Long>> mapPartitioning;
     private Map<Long, Map<String, Long>> mapPartitioning2;
 
-
-    public PartitioningRunnable(Main main, List<List<String>> wordGroupList) {
+    public PartitioningRunnable(Main main, File inputFile) {
         this.main = main;
-        this.wordGroupList = wordGroupList;
         calcMap = true;
+        mapPartitioning = new HashMap<>();
+        this.inputFile = inputFile;
     }
 
     public PartitioningRunnable(Main main, Map<Long, Map<String, Long>> mapPartitioning, Map<Long, Map<String, Long>> mapPartitioning2) {
@@ -38,7 +39,7 @@ public class PartitioningRunnable implements Runnable {
     @Override
     public void run() {
         threadID = Thread.currentThread().getId();
-//        System.out.println("Thread " + threadID % 4 + " running");
+//        System.out.println("Thread " + threadID + " running");
         try {
             if (calcMap) {
                 main.callback(calcMap());
@@ -67,8 +68,6 @@ public class PartitioningRunnable implements Runnable {
                 if (mapGroup.containsKey(word)) {
                     if (mapGroup.get(word) != null)
                         try {
-                            if(groupID.equals(new Long(1096901835)) && word.equals("sa"))
-                                System.out.println(word);
                             mapGroup.put(word, mapGroup.get(word) + mapPartitioning2.get(groupID).get(word));
                         } catch (Exception e) {
                             e.printStackTrace();
@@ -82,8 +81,9 @@ public class PartitioningRunnable implements Runnable {
 
     private Map<Long, Map<String, Long>> calcMap() throws IOException {
         String line;
+        int numLine = 0;
         String[] values;
-        BufferedReader bufferedPart = new BufferedReader(new FileReader(main.getPartPath() + (threadID % main.getNumberOfProcessors())));
+        BufferedReader bufferedPart = new BufferedReader(new FileReader(inputFile));
         Map<String, Long> mapGroup;
         mapPartitioning = new HashMap<>();
 
@@ -96,83 +96,45 @@ public class PartitioningRunnable implements Runnable {
         stemmersMap.put("es", new spanishStemmer());
         stemmersMap.put("en", new englishStemmer());
 
-        Map<Integer, Map<Long, List<String>>> mapPartitioningLists = new HashMap<>();
-        for (int i = 0; i < main.getNumberOfProcessors(); i++) {
-            mapPartitioningLists.put(i, (Map) new HashMap<>());
-        }
-
-        for (List<String> wordList : wordGroupList) {
-            Long groupID = null;
-            String lang = wordList.get(1);
-            try {
-                groupID = Long.parseLong(String.valueOf(wordList.get(0)));
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-                continue;
-            }
-
-            if (mapPartitioning.containsKey(groupID)) {
-                mapGroup = mapPartitioning.get(groupID);
-            } else {
-                mapGroup = new HashMap<>();
-                mapPartitioning.put(groupID, mapGroup);
-            }
-
-            wordList = wordList.subList(2, wordList.size());
-
-            for (String word : wordList) {
-                if (word.length() == 0) {
-                    continue;
-                }
-
-                SnowballStemmer currentStemmer = null;
-                if (stemmersMap.containsKey(lang)) {
-                    currentStemmer = stemmersMap.get(lang);
-                } else {
-                    currentStemmer = genericStemmer;
-                }
-                if (StopWords.getInstance().isStopWord(word)) {
-                    continue;
-                }
-                currentStemmer.setCurrent(word);
-                currentStemmer.stem();
-                String stemmed = currentStemmer.getCurrent();
-
-                if (mapGroup.containsKey(stemmed)) {
-                    mapGroup.put(stemmed, mapGroup.get(stemmed) + 1);
-                } else {
-                    mapGroup.put(stemmed, new Long(1));
-                }
-            }
-        }
-
         while ((line = bufferedPart.readLine()) != null) {
-
-            boolean validLine = true;
-            values = line.split(",");
-            List<String> words;
-
-            if(values.length > 2) {
-
-                Long groupID = null;
-                try {
-                    groupID = Long.parseLong(values[0]);
-                } catch (NumberFormatException e) {
-                    e.printStackTrace();
-                    continue;
-                }
-
+            if ((numLine % main.getNumberOfProcessors()) == (threadID % main.getNumberOfProcessors())) {
+                values = line.split(",");
                 String lang;
-                lang = values[1];
-                for (int i = 3; i < values.length ; i++) {
-                    values[2] = values[2] + "," + values[i]; // TODO use string builder here
-                }
 
-                words = Arrays.asList(values[2].split(","));
-                List<String> wordList = new ArrayList<>();
+                if (values.length > 4) {
+                    if (values.length > 5) {
+                        lang = values[values.length - 1];
+                        for (int i = 4; i < values.length - 1; i++) {
+                            values[3] = values[3] + values[i]; // TODO use string builder here
+                        }
+                    } else {
+                        try {
+                            lang = values[4];
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            lang = "unknown";
+                        }
+                    }
 
-                if (words.size() >= 1) {
-                    if (validLine) {
+                    String[] words = values[3].replaceAll("\\n", " ").replaceAll("[^\b a-zA-Z0-9'а-яА-Я]", "").trim().toLowerCase().split(" ");
+                    String groupIDstring = values[2];
+                    Long groupID;
+                    if (words != null && words.length >= 1) {
+                        try {
+                            groupID = Long.parseLong(groupIDstring);
+                        } catch (NumberFormatException e) {
+//                            e.printStackTrace();
+                            numLine++;
+                            continue;
+                        }
+
+                        if (mapPartitioning.containsKey(groupID)) {
+                            mapGroup = mapPartitioning.get(groupID);
+                        } else {
+                            mapGroup = new HashMap<>();
+                            mapPartitioning.put(groupID, mapGroup);
+                        }
+
                         for (String word : words) {
                             if (word.length() == 0) {
                                 continue;
@@ -190,26 +152,17 @@ public class PartitioningRunnable implements Runnable {
                             currentStemmer.setCurrent(word);
                             currentStemmer.stem();
                             String stemmed = currentStemmer.getCurrent();
-                            wordList.add(stemmed);
+
+                            if (mapGroup.containsKey(stemmed)) {
+                                mapGroup.put(stemmed, mapGroup.get(stemmed) + 1);
+                            } else {
+                                mapGroup.put(stemmed, new Long(1));
+                            }
                         }
                     }
                 }
-
-                if (mapPartitioning.containsKey(groupID)) {
-                    mapGroup = mapPartitioning.get(groupID);
-                } else {
-                    mapGroup = new HashMap<>();
-                    mapPartitioning.put(groupID, mapGroup);
-                }
-
-                for (String word : wordList) {
-                    if (mapGroup.containsKey(word)) {
-                        mapGroup.put(word, mapGroup.get(word) + 1);
-                    } else {
-                        mapGroup.put(word, new Long(1));
-                    }
-                }
             }
+            numLine++;
         }
         return mapPartitioning;
     }
